07aa274093ac28c8cc060a1a84128eefe64dcb88
Ángel Marín <angel.marin.mahm@gmail.com>
2026-08-28 02:02:38 +0000

    actualizacion por desuso de mysql

--- install-app.sh
+++ install-app.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Install Apache Web Server and PHP
-yum install -y httpd mysql php
+# 1. Actualizar el gestor de paquetes
+dnf update -y
 
-# Download Lab files
+# 2. Instalar Apache, PHP y el cliente MariaDB compatible con MySQL
+dnf install -y httpd php php-mysqlnd mariadb105
+
+# 3. Descargar los archivos del laboratorio
 wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-RESTRT-1/267-lab-NF-build-vpc-web-server/s3/lab-app.zip
 unzip lab-app.zip -d /var/www/html/
 
-# Turn on web server
-chkconfig httpd on
-service httpd start
+# 4. Encender y habilitar el servidor web Apache usando systemctl (El estándar moderno)
+systemctl start httpd
+systemctl enable httpd
